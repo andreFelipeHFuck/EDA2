e01816e9cc298b5a5753eec288eb8b92a4dbdeae
Lucas Guilherme Tascheck <89746181+Lucas-Tascheck@users.noreply.github.com>
2023-06-29 17:25:22 -0300

    mostrar grafico das avores (AVL, ARN)

---
+++ arvores/grafico.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+  
+# create data
+arquivoARN = open("/home/lucas/Documentos/EDA2/trabalho-final/EDA2-main/arvores/arvoreRubroNegra.txt", "r")
+arquivoAVL = open("/home/lucas/Documentos/EDA2/trabalho-final/EDA2-main/arvores/arvoreAVL.txt", "r")
+dadosARN = arquivoARN.read()
+dadosAVL = arquivoAVL.read()
+
+yARN = dadosARN.split(";")
+yAVL = dadosAVL.split(";")
+print(yAVL)
+
+tamanhoARN = len(yARN)
+tamanhoAVL = len(yAVL)
+i = 0
+vetorInserirARNx = []
+vetorInserirARNy = []
+vetorRemoverARNx = []
+vetorRemoverARNy = []
+
+vetorInserirAVLx = []
+vetorInserirAVLy = []
+vetorRemoverAVLx = []
+vetorRemoverAVLy = []
+
+while i < tamanhoARN -1:
+    split = yARN[i].split(":")
+    if(split[0] == 'I'):
+        vetorInserirARNx.append(split[1])
+        vetorInserirARNy.append(split[2])
+    else:
+        vetorRemoverARNx.append(split[1])
+        vetorRemoverARNy.append(split[2])
+    i+=1
+
+i = 0
+while i < tamanhoAVL -1:
+    split = yAVL[i].split(":")
+    if(split[0] == 'I'):
+        vetorInserirAVLx.append(split[1])
+        vetorInserirAVLy.append(split[2])
+    else:
+        vetorRemoverAVLx.append(split[1])
+        vetorRemoverAVLy.append(split[2])
+    i+=1
+
+
+
+# plot lines
+plt.subplot(1, 2, 1)
+plt.plot(np.array(vetorInserirARNx), np.array(vetorInserirARNy), label = 'Arvore Rubro Negra')
+plt.plot(vetorInserirAVLx, vetorInserirAVLy, label = 'Arvore AVL')
+#INSERIR ARVORE B AQUI
+plt.title("INSERÇÃO")
+
+plt.subplot(1, 2, 2)
+plt.plot(np.array(vetorRemoverARNx), np.array(vetorRemoverARNy), label = 'Arvore Rubro Negra')
+plt.plot(vetorRemoverAVLx, vetorRemoverAVLy, label = 'Arvore AVL')
+#INSERIR ARVORE B AQUI
+plt.title("REMOÇÃO")
+
+
+plt.legend()
+plt.show()
